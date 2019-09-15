ad6dd76dfde73f102b6ca61863bde41eacdb1365
JukeFA <45007798+JukeFA@users.noreply.github.com>
2019-09-14 23:48:15 -0600

    Update script.js

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -51,9 +51,9 @@ console.log(firstName + ' ' + lastName);
 */
 
 /**********
- * Basic Operators
- */
-
+* Basic Operators
+*/
+/*
 var year, yearJohn, yearMark
 now = 2018;
 ageJohn = 28;
@@ -76,12 +76,57 @@ var johnOlder = ageJohn > ageMark;
 console.log(johnOlder);
 
 //typeof operator
+
 console.log(typeof johnOlder);
 console.log(typeof ageJohn);
 console.log(typeof 'mark is older than john');
 var x;
 console.log(typeof x);
+*/
 
+/**********
+* Basic Operators
+*/
+/*
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple Operators
+var isFullAge = now - yearJohn >= fullAge; //true
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 -6 // 32 - 6 // 26
+console.log(x);
+
+// More Operators
+x *= 2;
+console.log(x);
+
+x += 10;
+console.log(x);
+*/
+
+var massMark = 78;
+var heightMark = 1.69;
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var bmiMark = massMark / (heightMark * heightMark);
+var bmiJohn = massJohn / (heightJohn * heightJohn);
+
+var markHigherBMI = bmiMark > bmiJohn;
+
+console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
 
 
 
