7563a9ff51917126bfe6e29b619aace8b9eb13bd
JukeFA <juke1229@gmail.com>
2019-10-20 15:39:16 -0400

    Ternary Operators and switch statements

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -186,3 +186,33 @@ if (age < 13) {
     console.log(firstName + ' is a man.');
 } */
 
+
+/**********
+The Ternary Operator and Switch Statements
+*/
+/* 
+var firstName = 'John';
+var age = 16;
+//Ternary Operator
+age >= 18 ? console.log(firstName + ' drinks beer.')
+: console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+//Switch Statement
+var job = 'teacher';
+switch (job) {
+    case 'teacher':
+        console.log(firstName + ' teaches kids how to code');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives uber in Lisbon');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beautiful websites.');
+        break;
+    default:
+        console.log(firstName + ' does something else.');
+}
+ */
