1bfad518910f6ffdf679237ecee29368301b9dc9
JukeFA <juke1229@gmail.com>
2019-11-08 18:00:51 -0500

    Scope and Scope chain

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,19 +1,30 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// functions
+calculateAge(1965);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
 
 
+//retirement(1965);
+var retirement = function(year) {
+    console.log(65 - (2016-year));
+}
 
 
+// Variables
+var age = 23;
+console.log(age);
 
-
-
-
-
-
-
-
-
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
@@ -40,9 +51,9 @@ function first() {
 
 
 
-// Example to show the differece between execution stack and scope chain
+// Example to show the difference between execution stack and scope chain
 
-/*
+
 var a = 'Hello!';
 first();
 
@@ -58,9 +69,9 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    console.log(a + d);
 }
-*/
+
 
 
 
