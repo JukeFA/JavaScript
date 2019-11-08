53d2a5184cc8ba7acbef975b6c4cb522e8e8741f
JukeFA <juke1229@gmail.com>
2019-11-08 15:01:05 -0500

    Code Challenge part 1

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -502,18 +502,33 @@ for (var i = john.length - 1 ; i >= 0; i-- ) {
 /**********
 coding challenge 5
 */
-var bill = [ ];
-var tip = [ ];
 
+var john  = {
+    fullName: 'John Smith',
+    bills: [124, 48, 268, 180, 42],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
 
+        for (var i = 0; i < this.bills.length; i++) {
+            //determine the percentage based on tipping rules
+            var percentage;
+            var bill = this.bills[i];
 
-var tipCalculator = function (bill) {
-    switch (bill) {
-        case bill < 50 :
-            return bill * .2;
-        case bill >=50 && bill <= 200 :
-            return bill * .15 ; 
-        case bill > 200 :
-            return bill * .1;
+            if (this.bills[i] < 50) {
+                percentage = .2;
+            } else if (this.bills[i] >= 50 && this.bills[i] < 200) {
+                percentage = .15;
+            } else { 
+                percentage = .1;
+            }
+
+            // add results to the corresponding arrays
+            this.tips [i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
     }
 }
+
+john.calcTips();
+console.log(john);
