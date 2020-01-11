8e69df8b1bbb539e648e83d9734067576f98b27e
JukeFA <45007798+JukeFA@users.noreply.github.com>
2020-01-10 22:47:30 -0500

    State Variables

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,7 +9,7 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
@@ -17,34 +17,52 @@ init();
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 // var x = document.querySelector('#score-0').textContent;
 
-
-
-
 // For rolling the dice
 document.querySelector('.btn-roll').addEventListener('click', function() {
-    
-    //1. Random Number
-    var dice = Math.floor(Math.random() * 6) + 1;
+    if(gamePlaying) {
+        //1. Random Number
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-    //2. display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice +'.png';
+        //2. display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice +'.png';
 
-    //3. update the round score IF the rolled number was NOT a 1
-    if (dice !== 1) {
-        //Add score
-        roundScore += dice; // roundScore = roundScore + dice
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-        //Next player
-        nextPlayer();
+        //3. update the round score IF the rolled number was NOT a 1
+        if (dice !== 1) {
+            //Add score
+            roundScore += dice; // roundScore = roundScore + dice
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //Next player
+            nextPlayer();
+        }
     }
+    
 })
 
 
 // For clicking the hold button
 document.querySelector('.btn-hold').addEventListener('click', function() {
+    if(gamePlaying) {
+        // Add current score to GLOBAl Score
+        scores[activePlayer] += roundScore;
+    
+        // Update UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        // Check if player won
+        if (scores[activePlayer] >= 100) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+        // Next player
+        nextPlayer()
+        }
+    }
     // Add current score to GLOBAl Score
     scores[activePlayer] += roundScore;
     
@@ -57,6 +75,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('player-' + activePlayer + '-panel').classList.remove('active');
+        gamePlaying = false;
     } else {
     // Next player
     nextPlayer()
@@ -72,6 +91,7 @@ function init() {
     scores = [0, 0];
     roundScore = 0;
     activePlayer = 0;
+    gamePlaying = true;
 
     document.querySelector('.dice').style.display = 'none';
     document.getElementById('score-0').textContent = '0';
