1d59baa61485180d64d8b2d3f63124cdb0157f00
JukeFA <45007798+JukeFA@users.noreply.github.com>
2020-01-10 22:40:31 -0500

    Initialization Function and New Game button

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -11,19 +11,13 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 // var x = document.querySelector('#score-0').textContent;
 
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+
 
 
 // For rolling the dice
@@ -46,7 +40,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         //Next player
         nextPlayer();
     }
-});
+})
 
 
 // For clicking the hold button
@@ -68,10 +62,34 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     nextPlayer()
     }
 
-});
+})
+
+// Clicking New Game
+document.querySelector('.btn-new').addEventListener('click', init)
 
 
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
 
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
+    document.querySelector('.player-0-panel').classList.add('active');
+}
 
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -82,4 +100,5 @@ function nextPlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active');
     document.querySelector('.dice').style.display = 'none';
 }
+
 
