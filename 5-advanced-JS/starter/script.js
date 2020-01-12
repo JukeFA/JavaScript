9c22c1039a276d6d9732df310f9988859e06db5c
JukeFA <45007798+JukeFA@users.noreply.github.com>
2020-01-12 16:34:57 -0500

    Object.create

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,6 +1,6 @@
 // Function Constructor
 
-var john = {
+/* var john = {
     name: 'John',
     yearOfBirth: 1990,
     job: 'teacher'
@@ -26,8 +26,23 @@ mark.calculateAge()
 
 console.log(john.lastName);
 console.log(jane.lastName);
-console.log(mark.lastName);
+console.log(mark.lastName); */
 
+//Object.create
 
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
 
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
 
+var jane = Object.create(personProto, {
+    name: { value:'Jane' },
+    yearOfBirthP: { value: 1969 },
+    job: { value: 'designer' },
+})
