a65b7db42c60fb429e20fdfd926855a3b284ac98
JukeFA <45007798+JukeFA@users.noreply.github.com>
2020-01-22 16:58:13 -0500

    Budget update UI

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,4 +1,4 @@
-// BUDGET CONTROLLER
+//** BUDGET CONTROLLER
 var budgetController = (function() {  
 
     var Expense = function(id, description, value) {
@@ -96,7 +96,7 @@ var budgetController = (function() {
 
 
 
-// UI CONTROLLER
+// ** UI CONTROLLER
 var UIController = (function() {
     
     var DOMstrings = {
@@ -105,14 +105,18 @@ var UIController = (function() {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expenseLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     }
 
     return {
         getInput: function() {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
-                descrption: document.querySelector(DOMstrings.inputDescription).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
                 value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             }
         },
@@ -151,6 +155,25 @@ var UIController = (function() {
             fieldsArr[0].focus();
         },
 
+        displayBudget: function(obj) {
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc
+            document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage
+
+
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%'
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---'
+
+            }
+
+
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -161,7 +184,7 @@ var UIController = (function() {
 
 
 
-// GLOBAL APP CONTROLLER
+//** GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
     var setupEventListeners = function() {
@@ -183,7 +206,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         // TODO 2. Return the budget
         var budget = budgetCtrl.getBudget();
         // TODO 3. Display the budget on the UI
-        console.log(budget)
+        UICtrl.displayBudget(budget)
     }
 
     var ctrlAddItem = function() {
@@ -195,7 +218,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
 
         // TODO 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.descrption, input.value)
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value)
 
         // TODO 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type)
@@ -213,6 +236,11 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.')
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1})
             setupEventListeners()
         }
     }
