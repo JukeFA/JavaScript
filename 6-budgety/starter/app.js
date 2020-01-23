297575d630b1ab1af2550e051f332f2db8ddbcef
JukeFA <45007798+JukeFA@users.noreply.github.com>
2020-01-22 21:44:03 -0500

    Improving UX

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -179,6 +179,12 @@ var UIController = (function() {
 
     }
 
+    var nodeListForEach = function (list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    }
+
     return {
         getInput: function() {
             return {
@@ -248,12 +254,6 @@ var UIController = (function() {
             
             var fields = document.querySelectorAll(DOMstrings.expensesPercentageLabel)
 
-            var nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i)
-                }
-            }
-
             nodeListForEach(fields, function(current, index) {
 
                 if (percentages[index] > 0) {
@@ -276,6 +276,21 @@ var UIController = (function() {
             year = now.getFullYear()
             document.querySelector(DOMstrings.datelabel).textContent = months[month] + ' ' + year
 
+        },
+
+        changedType: function() {
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue
+            )
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus')
+            })
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red')
         },
 
         getDOMstrings: function() {
@@ -303,6 +318,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         })
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem)
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType)
     }
 
     var updatePercentages = function() {
