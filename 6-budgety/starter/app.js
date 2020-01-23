ed01895d9d5bc1451e103d3d2b8fbdb163dcac5c
JukeFA <45007798+JukeFA@users.noreply.github.com>
2020-01-22 19:58:54 -0500

    Deleting Item from UI

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -158,6 +158,12 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml)
         },
 
+        deleteListItem: function(selectorID) {
+            var el = document.getElementById(selectorID)
+
+            el.parentNode.removeChild(el)
+        },
+
         clearFields: function() {
             var field, fieldsArr
 
@@ -266,10 +272,10 @@ var controller = (function(budgetCtrl, UICtrl) {
             budgetCtrl.deleteItem(type, ID)
 
             //TODO 2. Delete the item from the UI
-
+            UICtrl.deleteListItem(itemID)
 
             //TODO 3. Update and show the budget
-
+            updateBudget()
 
         }
     }
