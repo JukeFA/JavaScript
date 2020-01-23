3db0bccca385c1fe98eeb52fcbf999fa4179f0b9
JukeFA <45007798+JukeFA@users.noreply.github.com>
2020-01-22 21:25:28 -0500

    display current month and year

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -154,7 +154,8 @@ var UIController = (function() {
         expenseLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercentageLabel: '.item__percentage'
+        expensesPercentageLabel: '.item__percentage',
+        datelabel: '.budget__title--month'
     }
 
     var formatNumber = function(num, type) {
@@ -264,6 +265,19 @@ var UIController = (function() {
 
         },
 
+        displayMonth: function() {
+            var now, year, month
+            
+            now = new Date()
+            
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+            month = now.getMonth()
+
+            year = now.getFullYear()
+            document.querySelector(DOMstrings.datelabel).textContent = months[month] + ' ' + year
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -370,6 +384,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.')
+            UICtrl.displayMonth()
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
