ec4f03494090a73678e4eee3433cecfa86dc197f
JukeFA <45007798+JukeFA@users.noreply.github.com>
2020-02-02 14:35:11 -0500

    Recipe View Pt1

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search'
 import Recipe from './models/Recipe'
 import * as searchView from './views/searchView'
+import * as recipeView from './views/recipeView'
 import { elements, renderLoader, clearLoader } from './views/base'
 
 /**Global state of the app
@@ -62,7 +63,9 @@ const controlRecipe = async () => {
     console.log(id)
 
     if (id) {
-        // Prepare UI for changes //TODO 
+        // Prepare UI for changes 
+        recipeView.clearRecipe()
+        renderLoader(elements.recipe)
 
         // Create New recipe object
         state.recipe = new Recipe(id)
@@ -76,8 +79,9 @@ const controlRecipe = async () => {
             state.recipe.calcTime()
             state.recipe.calcServings()
 
-            // Render the recipe //TODO
-            console.log(state.recipe)
+            // Render the recipe 
+            clearLoader();
+            recipeView.renderRecipe(state.recipe)
         } catch (err) {
             alert('Error processing recipe!')
         }
--- 9-forkify/starter/src/js/models/Recipe.js
+++ 9-forkify/starter/src/js/models/Recipe.js
@@ -33,6 +33,7 @@ export default class Recipe {
     parseIngredients() {
         const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds', 'pound']
         const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup','lb', 'lb']
+        const units = [...unitsShort, 'kg', 'g']
         const newIngredients = this.ingredients.map(el => {
             // 1) Uniform Units
             let ingredient = el.toLowerCase()
@@ -45,7 +46,7 @@ export default class Recipe {
 
             // 3) Parse Ingredients into count, unit, and ingredient 
             const arrIng = ingredient.split(' ')
-            const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2))
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2))
 
             let objIng
             if (unitIndex > -1) {
